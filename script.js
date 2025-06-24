32824b9145441fc78931798b9b3009b4041902e2
KabeloMaps <mpnkab004@myuct.ac.za>
2025-06-24 18:08:47 +0200

    Fininshed with the general layout for the header and preview section

---
+++ script.js
@@ -0,0 +1,11 @@
+const LiveDemo_button = document.getElementById("btn-demo");
+
+LiveDemo_button.addEventListener("click", () => {
+  window.open("https://huddle-welcome-page.netlify.app", "_blank");
+});
+
+const Github_button = document.getElementById("btn-github");
+
+Github_button.addEventListener("click", () => {
+  window.open("https://github.com/KabeloMaps/Huddle-Landing-Page-", "_blank");
+});
