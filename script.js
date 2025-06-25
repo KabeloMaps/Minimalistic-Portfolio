ab681675e7eb483eec510b764c1f3accaaf1a53e
KabeloMaps <mpnkab004@myuct.ac.za>
2025-06-25 17:03:09 +0200

    Added all the projects

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const LiveDemo_button = document.getElementById("btn-demo");
+// const LiveDemo_button = document.getElementsByClassName("btn-demo");
 
-LiveDemo_button.addEventListener("click", () => {
-  window.open("https://huddle-welcome-page.netlify.app", "_blank");
+// LiveDemo_button.addEventListener("click", () => {
+//   window.open("https://huddle-welcome-page.netlify.app", "_blank");
+// });
+
+// const Github_button = document.getElementsByClassName("btn-github");
+
+// Github_button.addEventListener("click", () => {
+//   window.open("https://github.com/KabeloMaps/Huddle-Landing-Page-", "_blank");
+// });
+
+document.querySelectorAll(".btn-demo").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    window.open(btn.dataset.link, "_blank");
+  });
 });
 
-const Github_button = document.getElementById("btn-github");
-
-Github_button.addEventListener("click", () => {
-  window.open("https://github.com/KabeloMaps/Huddle-Landing-Page-", "_blank");
+document.querySelectorAll(".btn-github").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    window.open(btn.dataset.link, "_blank");
+  });
 });
