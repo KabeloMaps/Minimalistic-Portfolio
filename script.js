41a68e9b5902bc70fccc4be4280c70267be2ed27
KabeloMaps <mpnkab004@myuct.ac.za>
2025-07-04 12:20:41 +0200

    Added the Contoso University Application

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// const LiveDemo_button = document.getElementsByClassName("btn-demo");
-
-// LiveDemo_button.addEventListener("click", () => {
-//   window.open("https://huddle-welcome-page.netlify.app", "_blank");
-// });
-
-// const Github_button = document.getElementsByClassName("btn-github");
-
-// Github_button.addEventListener("click", () => {
-//   window.open("https://github.com/KabeloMaps/Huddle-Landing-Page-", "_blank");
-// });
-
 document.querySelectorAll(".btn-demo").forEach((btn) => {
   btn.addEventListener("click", () => {
     window.open(btn.dataset.link, "_blank");
@@ -21,3 +9,5 @@ document.querySelectorAll(".btn-github").forEach((btn) => {
     window.open(btn.dataset.link, "_blank");
   });
 });
+
+window.history.replaceState({}, document.title, window.location.pathname);
